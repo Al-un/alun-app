aeefe976a96191f2b966b56064853915a6b50e0c
Al-un <alun.sng@gmail.com>
2020-04-26 17:11:19 +0900

    Update API calls

--- api/user.ts
+++ api/user.ts
@@ -1,16 +1,42 @@
 import { SuccessfulLogin } from '~/models/user'
 
+const HOST = 'http://localhost:8080'
 const API_URL_USER = 'http://localhost:8000'
 
 export const userApi = {
   register: async (email: string): Promise<void> => {
     try {
-      await fetch(`${API_URL_USER}/users/v1/register`, {
+      const resp = await fetch(`${API_URL_USER}/users/v1/password/request`, {
         method: 'POST',
         mode: 'cors',
-        headers: { 'Content-Type': 'appplication/json' },
-        body: JSON.stringify({ email })
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email,
+          redirectUrl: `${HOST}/user/password-update/?requestType=1&t=`,
+          requestType: 1
+        })
       })
+
+      console.debug(resp)
+    } catch (err) {
+      console.warn(err)
+    }
+  },
+
+  requestResetPassword: async (email: string): Promise<void> => {
+    try {
+      const resp = await fetch(`${API_URL_USER}/users/v1/password/request`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email,
+          redirectUrl: `${HOST}/user/password-update/?requestType=1&t=`,
+          requestType: 0
+        })
+      })
+
+      console.debug(resp)
     } catch (err) {
       console.warn(err)
     }
@@ -26,12 +52,14 @@ export const userApi = {
       : { token, password }
 
     try {
-      await fetch(`${API_URL_USER}/users/v1/password/update`, {
+      const resp = await fetch(`${API_URL_USER}/users/v1/password/update`, {
         method: 'POST',
         mode: 'cors',
-        headers: { 'Content-Type': 'appplication/json' },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
       })
+
+      console.debug(resp)
     } catch (err) {
       console.warn(err)
     }
@@ -40,8 +68,8 @@ export const userApi = {
   login: async (email: string, password: string): Promise<SuccessfulLogin> => {
     const resp = await fetch(`${API_URL_USER}/users/v1/login`, {
       method: 'POST',
-      // mode: 'cors',
-      headers: { 'Content-Type': 'appplication/json' },
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
     })
     const success: SuccessfulLogin = await resp.json()
