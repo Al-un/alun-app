029205aaf293945f01a953e774743e0ecdf795d9
Al-un <alun.sng@gmail.com>
2020-05-20 00:22:06 +0900

    feat: update user API calls

--- api/user.ts
+++ api/user.ts
@@ -1,44 +1,76 @@
-import { Credentials, LoginAPISuccess, LoginResult } from '~/models/user'
+import { Credentials, LoginAPISuccess } from '~/models/user'
+import { USER_PWD_REQ, HTTP, API } from '~/constants'
+import { ApiErrorOr } from '~/models'
 
 const BASE_URL = process.env.ALUN_BASE_URL
 const API_URL_USER = process.env.ALUN_API_URL_USER
 
 export const userApi = {
-  register: async (email: string): Promise<void> => {
+  register: async (email: string): Promise<ApiErrorOr<undefined>> => {
     try {
       const resp = await fetch(`${API_URL_USER}/v1/password/request`, {
-        method: 'POST',
+        method: HTTP.METHOD.POST,
         mode: 'cors',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           email,
-          redirectUrl: `${BASE_URL}/user/password-update/?requestType=1&t=`,
-          requestType: 1
+          redirectUrl: `${BASE_URL}/user/password-update/?requestType=${USER_PWD_REQ.NEW_USER}&t=`,
+          requestType: USER_PWD_REQ.NEW_USER
         })
       })
+      const { status } = resp
 
-      console.debug(resp)
-    } catch (err) {
-      console.warn(err)
+      // All good!
+      if (status === HTTP.STATUS.NO_CONTENT) {
+        return undefined
+      }
+
+      // Fetch the content to get error detail
+      const content = await resp.json()
+      // Potentially an identified error
+      if (
+        HTTP.STATUS.BAD_REQUEST === status &&
+        API.MSGS.USER_EMAIL_ALREADY_TAKEN === content.code
+      ) {
+        return { status, code: content.code }
+      }
+
+      return {
+        status,
+        code: API.MSGS.UNKNOWN,
+        error: content
+      }
+    } catch (error) {
+      return { error }
     }
   },
 
-  requestResetPassword: async (email: string): Promise<void> => {
+  requestResetPassword: async (
+    email: string
+  ): Promise<ApiErrorOr<undefined>> => {
     try {
       const resp = await fetch(`${API_URL_USER}/v1/password/request`, {
-        method: 'POST',
+        method: HTTP.METHOD.POST,
         mode: 'cors',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           email,
-          redirectUrl: `${BASE_URL}/user/password-update/?requestType=1&t=`,
-          requestType: 0
+          redirectUrl: `${BASE_URL}/user/password-update/?requestType=${USER_PWD_REQ.RESET}&t=`,
+          requestType: USER_PWD_REQ.RESET
         })
       })
+      const { status } = resp
 
-      console.debug(resp)
-    } catch (err) {
-      console.warn(err)
+      // All good!
+      if (status === HTTP.STATUS.NO_CONTENT) {
+        return undefined
+      }
+
+      const error = await resp.json()
+
+      return { status, error }
+    } catch (error) {
+      return { error }
     }
   },
 
@@ -46,50 +78,76 @@ export const userApi = {
     token: string,
     password: string,
     username?: string
-  ): Promise<void> => {
+  ): Promise<ApiErrorOr<undefined>> => {
     const payload = username
       ? { token, password, username }
       : { token, password }
 
     try {
       const resp = await fetch(`${API_URL_USER}/v1/password/update`, {
-        method: 'POST',
+        method: HTTP.METHOD.POST,
         mode: 'cors',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
       })
+      const { status } = resp
 
-      console.debug(resp)
-    } catch (err) {
-      console.warn(err)
+      // All good!
+      if (status === HTTP.STATUS.NO_CONTENT) {
+        return undefined
+      }
+
+      const error = await resp.json()
+
+      return { status, error }
+    } catch (error) {
+      return { error }
     }
   },
 
-  login: async ({ email, password }: Credentials): Promise<LoginResult> => {
-    const resp = await fetch(`${API_URL_USER}/v1/login`, {
-      method: 'POST',
-      mode: 'cors',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password })
-    })
+  login: async ({
+    email,
+    password
+  }: Credentials): Promise<ApiErrorOr<LoginAPISuccess>> => {
+    try {
+      const resp = await fetch(`${API_URL_USER}/v1/login`, {
+        method: HTTP.METHOD.POST,
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+      })
+      const content = await resp.json()
 
-    if (resp.status === 200) {
-      const success: LoginAPISuccess = await resp.json()
-      return {
-        ...success,
-        success: true
+      switch (resp.status) {
+        case HTTP.STATUS.OK:
+          return content as LoginAPISuccess
+        case HTTP.STATUS.FORBIDDEN:
+          return { status: resp.status, code: API.MSGS.USER_LOGIN_FAILED }
+        default:
+          return { status: resp.status, error: content }
       }
-    } else if (resp.status === 403) {
-      return {
-        success: false,
-        token: ''
-      }
-    } else {
-      const pouet = await resp.json()
-      console.log(pouet)
-      throw new Error('failed')
+    } catch (error) {
+      return { error }
     }
   },
 
-  logout: async () => {}
+  logout: async (token: string): Promise<ApiErrorOr<undefined>> => {
+    try {
+      const resp = await fetch(`${API_URL_USER}/v1/logout`, {
+        method: HTTP.METHOD.POST,
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Token ${token}`
+        }
+      })
+      const content = await resp.json()
+
+      return resp.status === HTTP.STATUS.NO_CONTENT
+        ? undefined
+        : { status: resp.status, error: content }
+    } catch (error) {
+      return { error }
+    }
+  }
 }
