d1f2713b8a900b84c0d388f0c9696c47732e3652
Al-un <alun.sng@gmail.com>
2020-05-20 00:23:00 +0900

    refactor: move the composition.ts file down to components level, not elements level

--- components/user/composition.ts
+++ components/user/composition.ts
@@ -1,8 +1,10 @@
-import { SetupContext, computed, Ref } from '@vue/composition-api'
+import { SetupContext, computed, Ref, ref } from '@vue/composition-api'
 import { UnwrapRef } from '@vue/composition-api/dist/reactivity'
 
 import { userApi } from '~/api/user'
 import { Credentials } from '~/models/user'
+import { ApiCallStatus, ApiErrorOr } from '~/models'
+import { API } from '~/constants'
 
 export const useLogin = (
   ctx: SetupContext,
@@ -21,6 +23,33 @@ export const useLogin = (
     submit,
     status
   }
+}
+
+export const useRegister = (email: Ref<string>) => {
+  const status = ref<ApiCallStatus>('idle')
+
+  const submit = async () => {
+    try {
+      status.value = 'processing'
+      const result: ApiErrorOr<undefined> = await userApi.register(email.value)
+
+      if (!result) {
+        status.value = 'success'
+        return
+      }
+
+      if (API.MSGS.USER_EMAIL_ALREADY_TAKEN === result.code) {
+        status.value = 'failed'
+      } else {
+        status.value = 'error'
+      }
+    } catch (err) {
+      status.value = 'error'
+      console.warn('TODO ERROR:', err)
+    }
+  }
+
+  return { status, submit }
 }
 
 export const usePasswordRequest = (email: Ref<string>) => {
