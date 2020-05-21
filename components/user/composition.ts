a8d81e43851ec0734b80b331edfde7b7e40cf622
Al-un <alun.sng@gmail.com>
2020-05-22 00:24:58 +0900

    feat: update user auth/password logic

--- components/user/composition.ts
+++ components/user/composition.ts
@@ -2,31 +2,74 @@ import { SetupContext, computed, Ref, ref } from '@vue/composition-api'
 import { UnwrapRef } from '@vue/composition-api/dist/reactivity'
 
 import { userApi } from '~/api/user'
-import { Credentials } from '~/models/user'
-import { ApiCallStatus, ApiErrorOr } from '~/models'
+import {
+  ApiCallStatus,
+  ApiErrorOr,
+  Credentials,
+  LoginAPISuccess,
+  ApiError
+} from '~/models'
 import { API } from '~/constants'
+import { getAlertTypeApiCallStatus } from '~/utils'
+
+const useUserAuthFormBase = () => {
+  const status = ref<ApiCallStatus>('idle')
+  const alertType = computed(() => getAlertTypeApiCallStatus(status.value))
+  const isFormValid = ref(false)
+
+  return { status, alertType, isFormValid }
+}
 
 export const useLogin = (
   ctx: SetupContext,
   credentials: UnwrapRef<Credentials>
 ) => {
-  const status = computed(() => ctx.root.$store.state.user.loginStatus)
+  const { status, alertType, isFormValid } = useUserAuthFormBase()
 
   const submit = async () => {
-    await ctx.root.$store.dispatch('user/login', {
-      email: credentials.email,
-      password: credentials.password
-    })
+    try {
+      status.value = 'processing'
+      const result = await userApi.login(credentials)
+
+      // Yeah \o/
+      const resOk = result as LoginAPISuccess
+      if (resOk.token) {
+        ctx.root.$store.dispatch('saveToken', resOk.token)
+        status.value = 'success'
+
+        const nextPage = (ctx.root.$route.query.nextPage as string) || '/'
+
+        window.setTimeout(() => {
+          ctx.root.$router.replace({ path: nextPage })
+        }, 1000)
+        return
+      }
+
+      // Try again!
+      const resErr = result as ApiError
+      if (API.MSGS.USER_LOGIN_FAILED === resErr.code) {
+        status.value = 'failed'
+        return
+      }
+
+      // WTF error
+      status.value = 'error'
+    } catch (err) {
+      console.warn('loginalert ERROR:', err)
+      status.value = 'error'
+    }
   }
 
   return {
     submit,
-    status
+    status,
+    alertType,
+    isFormValid
   }
 }
 
 export const useRegister = (email: Ref<string>) => {
-  const status = ref<ApiCallStatus>('idle')
+  const { status, alertType, isFormValid } = useUserAuthFormBase()
 
   const submit = async () => {
     try {
@@ -44,36 +87,71 @@ export const useRegister = (email: Ref<string>) => {
         status.value = 'error'
       }
     } catch (err) {
+      console.warn('useRegister ERROR:', err)
       status.value = 'error'
-      console.warn('TODO ERROR:', err)
     }
   }
 
-  return { status, submit }
+  return { status, submit, alertType, isFormValid }
 }
 
 export const usePasswordRequest = (email: Ref<string>) => {
-  const request = async () => {
+  const { status, alertType, isFormValid } = useUserAuthFormBase()
+
+  const submit = async () => {
     try {
-      await userApi.requestResetPassword(email.value)
+      status.value = 'processing'
+      const result: ApiErrorOr<undefined> = await userApi.requestResetPassword(
+        email.value
+      )
+
+      if (!result) {
+        status.value = 'success'
+        return
+      }
+
+      status.value = 'error'
     } catch (err) {
-      console.warn('TODO ERROR', err)
+      console.warn('usePasswordRequest ERROR', err)
+      status.value = 'error'
     }
   }
 
-  return { request }
+  return { status, submit, alertType, isFormValid }
 }
 
-export const usePasswordUpdate = (ctx: SetupContext, password: Ref<string>) => {
-  const update = async () => {
+export const usePasswordUpdate = (
+  ctx: SetupContext,
+  password: Ref<string>,
+  username: Ref<string>
+) => {
+  const { status, alertType, isFormValid } = useUserAuthFormBase()
+
+  const isNewUser = ctx.root.$route.query.requestType
+    ? ctx.root.$route.query.requestType === '1'
+    : false
+
+  const submit = async () => {
     const token = ctx.root.$route.query.t as string
 
     try {
-      await userApi.updatePassword(token, password.value)
+      status.value = 'processing'
+      const result: ApiErrorOr<undefined> = await userApi.updatePassword(
+        token,
+        password.value,
+        username.value ? username.value : undefined
+      )
+
+      if (!result) {
+        status.value = 'success'
+        return
+      }
+
+      status.value = 'error'
     } catch (err) {
       console.warn('TODO ERROR', err)
     }
   }
 
-  return { update }
+  return { status, submit, alertType, isFormValid, isNewUser }
 }
