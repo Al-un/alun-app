9924cd365c49f7f43d929f96dc6595ce895c3ed1
Al-un <alun.sng@gmail.com>
2020-05-23 08:49:30 +0900

    feat: add empty pages for memos app and colour utility

---
+++ pages/apps/memos/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>TODO</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+interface Props {}
+
+export default defineComponent({
+  name: 'app-memos'
+})
+</script>
+
+<style lang="scss"></style>
---
+++ pages/utilities/colours.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>TODO</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+interface Props {}
+
+export default defineComponent({
+  name: 'util-colours'
+})
+</script>
+
+<style lang="scss"></style>
