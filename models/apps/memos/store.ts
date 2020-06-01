782e0012880f714721f7e58f874345bc55337ca8
Al-un <alun.sng@gmail.com>
2020-06-01 23:50:23 +0900

    feat: define RootGetters and RootComputedGetters to faciliate rootGetters definition in MemoActionContext

--- models/apps/memos/store.ts
+++ models/apps/memos/store.ts
@@ -1,10 +1,12 @@
 import { ActionContext } from 'vuex'
 
 import { Board } from './entity'
-import { RootState } from '~/models/store'
+import { RootState, RootComputedGetters } from '~/models/store'
 
 export interface MemosState {
   boards: Board[]
 }
 
-export type MemosActionCtx = ActionContext<MemosState, RootState>
+export interface MemosActionCtx extends ActionContext<MemosState, RootState> {
+  rootGetters: RootComputedGetters
+}
--- models/store.ts
+++ models/store.ts
@@ -1,4 +1,4 @@
-import { ActionContext } from 'vuex'
+import { ActionContext, GetterTree } from 'vuex'
 import { UserProfile } from './user'
 export interface RootState {
   token?: string
@@ -6,3 +6,13 @@ export interface RootState {
 }
 
 export type RootActionCtx = ActionContext<RootState, RootState>
+
+export interface RootGetters extends GetterTree<RootState, RootState> {
+  getTokenOrError: (state: RootState) => string
+  isLogged: (state: RootState) => boolean
+}
+
+export interface RootComputedGetters {
+  getTokenOrError: string
+  isLogged: boolean
+}
